4495207aa9bf515917b4109165eeffe8675e3674
Carolina Rosales <mrosalesr@est.ups.edu.ec>
2020-10-02 10:09:45 -0500

    Seminario Node.js + MongoDB

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const config = {
-    dbUrl: process.env.DB_URL || 'mongodb+srv://adminUPS:ups2020@cluster0.dclfj.gcp.mongodb.net/<database>?retryWrites=true&w=majority',
+    dbUrl: process.env.DB_URL || 'mongodb+srv://ups:ups2020@cluster0.9qbv2.gcp.mongodb.net/basededatosSeminario?retryWrites=true&w=majority',
     port: process.env.PORT || 5000,
     host: process.env.HOST || 'http://localhost',
     publicRoute: process.env.PUBLIC_ROUTE || '/',
